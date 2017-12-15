559e016e34b7bf4b78318ac724594066aeaf2066
ramprabhu <nramprabu@gmail.com>
2017-12-15 13:57:06 +0530

    adding validations

--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -4,6 +4,8 @@ class UserBook < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
+  validates :user_id, :book_id, :start_date, :return_date, presence: true
+
   def allow_five_book_per_user
     user = User.find(user_id)
     if user.books.size == MAX_BOOK_PER_USER
