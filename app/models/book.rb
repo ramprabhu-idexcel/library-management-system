51d1bdad68d9d5da5e510970871eb427bbbeaccf
ramprabhu <nramprabu@gmail.com>
2017-12-15 16:19:48 +0530

    User & Books search

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,13 @@ class Book < ApplicationRecord
   validates :number, presence: true, length: { maximum: 10 }
   validates :author, presence: true, length: { maximum: 50 }
   has_many :users, through: :user_books, dependent: :destroy
+  default_scope { order('created_at DESC') }
+
+  def self.search(search)
+    if search
+      where("name LIKE ? OR number LIKE ? OR author LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
+    else
+      all
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  default_scope { order('created_at DESC') }
   before_save { self.email = email.downcase }
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
@@ -6,11 +7,19 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
-  validates :phone, presence: true, length: { minimum: 10 }
+  validates :phone, presence: true, length: { minimum: 10 }, uniqueness: true
   has_many :user_books
   has_many :books, through: :user_books, dependent: :destroy
 
   def name
     "#{first_name} #{last_name}"
+  end
+
+  def self.search(search)
+    if search
+      where("first_name LIKE ? OR last_name LIKE ? OR email LIKE ? OR phone LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+    else
+      all
+    end
   end
 end
